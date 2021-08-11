7643c54e4a1c0a93d5b4bb5002ebea9f774e8d33
Siphiwe Kanyenda <84739465+Sips-maker@users.noreply.github.com>
2021-08-11 10:24:14 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "holberton.h"
 
 /**
  * read_textfile - Entry Point
@@ -8,32 +9,23 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file, rd, wr;
-	char *buf;
+	int fd, readed;
+	char *buff = malloc(sizeof(char *) * letters);
 
-	if (filename == NULL)
+	if (!buff)
 		return (0);
 
-	file = open(filename, O_RDONLY);
-
-	if (file == -1)
+	if (!filename)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters + 1);
-	if (buf == NULL)
+	fd = open(filename, O_RDONLY, 0600);
+	if (fd == -1)
 		return (0);
 
-	rd = read(file, buf, letters);
-	if (rd == -1)
-		return (0);
+	readed = read(fd, buff, letters);
+	write(STDOUT_FILENO, buff, readed);
 
-	buf[letters] = '\0';
-
-	wr = write(1, buf, rd);
-	if (wr == -1)
-		return (0);
-
-	close(file);
-	free(buf);
-	return (wr);
+	free(buff);
+	close(fd);
+	return (readed);
 }
