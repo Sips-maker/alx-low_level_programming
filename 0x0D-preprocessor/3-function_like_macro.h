4d3a421777c625e1ea624f32488f59275c9fe5c5
Siphiwe Kanyenda <84739465+Sips-maker@users.noreply.github.com>
2021-08-17 13:46:51 +0200

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,6 @@
-#ifndef _HEADER_
-#define _HEADER_
-#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
-#endif#ifndef _HEADER_
-#define _HEADER_
-#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+
 #endif
